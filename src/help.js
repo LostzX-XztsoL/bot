033b4c4b4882a3a49205fd65150d7db5a3222b30
LostzX-XztsoL <79033314+LostzX-XztsoL@users.noreply.github.com>
2021-02-14 11:36:42 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,130 @@
+const help = (prefix, pushname) => {
+	return `「 LostzX BOT 」
+
+ ✅ | Prefixo「 ${prefix} 」
+ ✅ | Status: Online
+ ✅ | Criado por: @lostzx.mp4
+
+「 Informações 」
+
+⚡| ${prefix}teste
+⚡| ${prefix}info
+⚡| ${prefix}owner
+⚡| ${prefix}ping
+⚡| ${prefix}blocklist
+
+「 Comandos de figurinhas 」
+
+⚡| ${prefix}sticker
+⚡| ${prefix}stiker
+⚡| ${prefix}stickergif
+⚡| ${prefix}stikergif
+⚡| ${prefix}toimg
+
+「 Comandos de áudio 」
+
+⚡| ${prefix}tts
+⚡| ${prefix}play
+
+「 Comandos de download 」
+
+⚡| ${prefix}play
+⚡| ${prefix}ytmp4
+⚡| ${prefix}apks
+
+「 Comandos +18 」
+
+🔞 | ${prefix}nsfwfeets
+🔞 | ${prefix}nsfwbobs
+🔞 | ${prefix}randomhentai
+🔞 | ${prefix}nsfwass
+🔞 | ${prefix}nsfwtrap
+🔞 | ${prefix}nsfwbelly
+🔞 | ${prefix}nsfwhegao
+🔞 | ${prefix}nsfwsidebobs
+🔞 | ${prefix}nsfwthighs
+🔞 | ${prefix}nsfwarmpits
+🔞 | ${prefix}pack
+🔞 | ${prefix}links
+
+「 Comandos de admin 」
+
+⚡| ${prefix}leave
+⚡| ${prefix}opengc
+⚡| ${prefix}closegc
+⚡| ${prefix}add
+⚡| ${prefix}kick
+⚡| ${prefix}welcome (1/0)
+⚡| ${prefix}tagall (1/2/3/4/5)
+⚡| ${prefix}nsfw (1/0)
+⚡| ${prefix}promote
+⚡| ${prefix}demote
+⚡| ${prefix}leveling (1/0)
+⚡| ${prefix}welcome (1/0)
+⚡| ${prefix}delete
+⚡| ${prefix}simih (1/0)
+
+ 「 Comandos de logos 」
+
+*Os servidores estão instáveis. Caso não envie,mande o comando denovo.*
+
+⚡| ${prefix}bpink
+⚡| ${prefix}marvellogo
+⚡| ${prefix}snowwrite
+⚡| ${prefix}3dtext
+⚡| ${prefix}ninjalogo
+⚡| ${prefix}water
+⚡| ${prefix}firetext
+⚡| ${prefix}logowolf
+⚡| ${prefix}logowolf2
+⚡| ${prefix}phlogo
+⚡| ${prefix}glitch
+⚡| ${prefix}neonlogo
+⚡| ${prefix}neonlogo2
+⚡| ${prefix}lionlogo
+⚡| ${prefix}jokerlogo
+⚡| ${prefix}shadow
+⚡| ${prefix}burnpaper
+⚡| ${prefix}coffee
+⚡| ${prefix}ovepaper
+⚡| ${prefix}woodblock
+⚡| ${prefix}qowheart
+⚡| ${prefix}mutgrass
+⚡| ${prefix}undergocean
+⚡| ${prefix}woodenboards
+⚡| ${prefix}wolfmetal
+⚡| ${prefix}metalictglow
+⚡| ${prefix}8bit
+⚡| ${prefix}ttp
+⚡| ${prefix}harrypotter
+⚡| ${prefix}pubglogo
+⚡| ${prefix}quotemaker
+
+ 「 Outros 」
+
+⚡| ${prefix}minigames
+⚡| ${prefix}wame
+⚡| ${prefix}tiktokstalk
+⚡| ${prefix}ocr
+⚡| ${prefix}calúnia
+
+ 「 Grupo do meu criador 」
+
+🐊 | Grupo de zoeira sem motivo,caso queira entrar leia as regras na descrição para não ser banido,experimente os comandos do bot que são disponíveis somente em grupos e divirta-se!
+
+https://chat.whatsapp.com/BuY1JEsaY5EJvr7Ku1ahQO
+
+「 Donate 」
+
+❤️ | use ${prefix}donate para fazer uma doação.
+
+「 Me adicionar 」
+
+🌪️ | Para me adicionar em um grupo,use ${prefix}adc
+
+                       by @lostzx.mp4
+                 wa.me/5511950438690`
+
+}
+
+exports.help = help
